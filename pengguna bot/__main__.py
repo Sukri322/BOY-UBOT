90438938c2ee43e413dd6bce157d5685cfcaa183
Sukri322 <88951877+Sukri322@users.noreply.github.com>
2021-08-15 11:12:57 +0800

    UBAH _main_.py

--- pengguna bot/__main__.py
+++ pengguna bot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⭐BOY-UBOT⭐ 🔥 V7.0 [AKTIF KONTOLLLL!!!!]")
+    f"⭐RAM-UBOT⭐ 🔥 V7.0 [AKTIF KONTOLLLL!!!!]")
 
 
 if len(argv) not in (1, 3, 4):
